76be092dfb098d1dfe63c57c85eb0670e6c8b906
ngkhanhlinh28 <134145534+ngkhanhlinh28@users.noreply.github.com>
2023-05-20 18:37:03 +0200

    New scene with map

--- TeamCandle2D/Assets/Scripts/TimeKeeper.cs
+++ TeamCandle2D/Assets/Scripts/TimeKeeper.cs
@@ -6,15 +6,34 @@ public class TimeKeeper : MonoBehaviour
 {
     private float startTime;
     public float pastTime = 0;
+    public AudioSource audioSource;
+    public AudioClip gamesound; 
+    public AudioClip deathsound;
+    public float volume; 
+    bool isAlive = true; 
+
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
+        audioSource.PlayOneShot (gamesound, volume);
     }
 
     // Update is called once per frame
     void Update()
     {
         pastTime = Time.time - startTime;
+        Die();
     }
+
+    void Die ()
+    {
+        if (pastTime >= 10 && isAlive)
+        {
+            audioSource.PlayOneShot (deathsound, volume);
+            Time.timeScale = 0; 
+            isAlive = false; 
+        }
+    }
+
 }
