b07fee332e2604759c4efa39afa73dbda9701794
flektor <gdrosin@gmail.com>
2023-05-20 16:39:16 +0200

    Added character sprites

--- TeamCandle2D/Assets/Scripts/CharacterController.cs
+++ TeamCandle2D/Assets/Scripts/CharacterController.cs
@@ -4,13 +4,28 @@ public class CharacterController : MonoBehaviour
 {
     Transform _transform;
 
+
+    Direction _direction;
     float _speed = .05f;
+
+   public enum Direction
+    {
+        Forward,
+        Backward,
+        Right,
+        Left
+    }
+
+    public delegate void OnChangeDirection(Direction direction);
+
+
+   public  OnChangeDirection OnChangeDirectionCallback;
 
     // Start is called before the first frame update
     void Start()
     {
         _transform = GetComponent<Transform>();
-
+        _direction = Direction.Forward;
     }
 
     // Update is called once per frame
@@ -43,34 +58,43 @@ public class CharacterController : MonoBehaviour
         }
 
     }
-
-
-
+     
     void MoveForward()
     {
        var pos =  _transform.position;
        pos.y += _speed;
        _transform.position = pos;
+       SetDirection(Direction.Forward);
     }
     void MoveBackward()
     {
         var pos = _transform.position;
         pos.y -= _speed;
         _transform.position = pos;
+        SetDirection(Direction.Backward);
     }
     void MoveRight()
     {
         var pos = _transform.position;
         pos.x += _speed;
         _transform.position = pos;
+        SetDirection(Direction.Right);
     }
     void MoveLeft()
     {
         var pos = _transform.position;
         pos.x -= _speed;
         _transform.position = pos;
+       SetDirection(Direction.Left);
     }
 
-
+    void SetDirection(Direction direction)
+    {
+        if (_direction != direction)
+        {
+            _direction = direction;
+            OnChangeDirectionCallback(direction);
+        }
+    }
 
 }
---
+++ TeamCandle2D/Assets/Scripts/CharacterSpiteController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class CharacterSpiteController : MonoBehaviour
+{ 
+    SpriteRenderer _spriteRenderer ;
+    public Sprite ForwardSprite;
+    public Sprite BackwardSprite;
+    public Sprite RightSprite;
+    public Sprite LeftSprite;
+
+    CharacterController.Direction _direction;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        var charachterController = GetComponent<CharacterController>();
+        charachterController.OnChangeDirectionCallback = SetCurrentSprite;
+    }
+     
+    void SetCurrentSprite(CharacterController.Direction direction)
+    {
+        if(direction == CharacterController.Direction.Forward)
+        {
+            _spriteRenderer.sprite = ForwardSprite;
+            return;
+        }
+        if (direction == CharacterController.Direction.Backward)
+        {
+            _spriteRenderer.sprite = BackwardSprite;
+            return;
+        }
+        if (direction == CharacterController.Direction.Right)
+        {
+            _spriteRenderer.sprite = RightSprite;
+            return;
+        }
+        if (direction == CharacterController.Direction.Left)
+        {
+            _spriteRenderer.sprite = LeftSprite;
+            return;
+        }
+
+    }
+ 
+ 
+}
