83bdfab1d9d88d84d910c77eb843767c3bb879f4
Sandroleon313 <sandroflei313@gmail.com>
2023-05-20 16:42:17 +0200

    Added an Enemy that moves between 2 points

---
+++ TeamCandle2D/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform startingPoint;
+    public Transform endingPoint;
+
+    private float progress = 0;
+
+    public float speed = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startingPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progress = progress + speed * Time.deltaTime;
+
+        Debug.Log(Mathf.Abs(Mathf.Sin(progress)));
+
+        float currentX = transform.position.x;
+
+        transform.position = startingPoint.position + ((endingPoint.position - startingPoint.position) * Mathf.Abs(Mathf.Sin(progress)));
+
+        float newX = transform.position.x;
+
+        if(newX < currentX)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name + " hit the enemy");
+    }
+}
