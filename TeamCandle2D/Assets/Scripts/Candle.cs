875d2078845b157ca88a5e8fecb7b07396388814
flektor <gdrosin@gmail.com>
2023-05-21 19:14:04 +0200

    updated candle timer

--- TeamCandle2D/Assets/Scripts/Candle.cs
+++ TeamCandle2D/Assets/Scripts/Candle.cs
@@ -1,57 +1,37 @@
-using System.Collections.Generic;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Candle : MonoBehaviour
-{ 
-    [SerializeField] List<Sprite> sprites = new ();
-
-    SpriteRenderer _spriteRenderer;
-
+{
+    [SerializeField] Sprite[] sprites;
+    SpriteRenderer spriteRenderer;
     int _currentIndex;
-    float _timeToNextSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-           
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Animate();
+    }
+
+    public void Animate()
+    {
+        StopCoroutine(nameof(SequenceStart));
+
         _currentIndex = 0;
-        if(sprites.Count > 0)
-        {
-            updateTimeToNextSprite();
-            SetCurrentSprite(sprites[_currentIndex]);
-        }
+        spriteRenderer.sprite = sprites[_currentIndex];
+        StartCoroutine(nameof(SequenceStart));
     }
 
-    private void Update()
-    { 
-        if(_currentIndex == sprites.Count)
+    IEnumerator SequenceStart()
+    {
+        var timeToNextSprite = +.3f + 60 / sprites.Length;
+
+        for (_currentIndex = 0; _currentIndex < sprites.Length; _currentIndex++)
         { 
-            SetCurrentSprite(sprites[_currentIndex]); 
-            return;
+            yield return new WaitForSeconds(timeToNextSprite);
+            spriteRenderer.sprite = sprites[_currentIndex];
         }
-
-        _timeToNextSprite -= Time.deltaTime;
-
-       if (_timeToNextSprite > 0)
-        {
-             return;
-        }
-
-        _currentIndex++;
-        SetCurrentSprite(sprites[_currentIndex]); 
-        updateTimeToNextSprite();          
     }
-  
-    void SetCurrentSprite(Sprite sprite)
-    {
-        _spriteRenderer.sprite = sprite;
-    }
-
-   void updateTimeToNextSprite()
-    {
-        _timeToNextSprite = 60 / sprites.Count + 1; 
-    }
-
-
 }
